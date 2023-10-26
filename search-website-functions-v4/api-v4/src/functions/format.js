8d8c2c9d287d394d97388ca4bd359ff22935364a
Joe Campbell <joe.campbell@fireman.company>
2023-10-26 11:03:08 +0100

    Create format.js

---
+++ search-website-functions-v4/api-v4/src/functions/format.js
@@ -0,0 +1,61 @@
+const { app } = require('@azure/functions');
+const { CONFIG } = require("../lib/config");
+const { readFacets, createFilterExpression } = require('../lib/azure-cognitive-search');
+const { SearchClient, AzureKeyCredential } = require("@azure/search-documents");
+
+// Create a SearchClient to send queries
+const client = new SearchClient(
+    `https://` + CONFIG.SearchServiceName + `.search.windows.net/`,
+    CONFIG.SearchIndexName,
+    new AzureKeyCredential(CONFIG.SearchApiQueryKey)
+);
+
+app.http('format', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+
+        context.log(`format request for url "${request.url}"`);
+
+        try {
+            var chunkSize = 20000;
+            var chuckResults = [];
+            const body = await request.json();
+            var extractText = ""
+            for (const result of body.body.responsev2.predictionOutput.results) {
+                for (const line of result.lines) {
+                    extractText = extractText + " " + line.text
+                }
+            }
+
+            
+
+            for (let i = 0; i < extractText.length; i += chunkSize) {
+                let chunk = extractText.substring(i, i + chunkSize);
+                chuckResults.push(chunk);
+            }
+            
+            return {
+                headers: {
+                    "Content-type": "application/json"
+                },
+                jsonBody: {
+                    charCount: extractText.length,
+                    results: chuckResults,
+                    resultsCount: chuckResults.length
+                    
+                }
+            };
+
+        } catch (error) {
+            return {
+                status: 500,
+                jsonBody: {
+                    innerStatusCode: error.statusCode || error.code,
+                    error: error.details || error.message,
+                    stack: error.stack
+                }
+            }
+        }
+    }
+});
