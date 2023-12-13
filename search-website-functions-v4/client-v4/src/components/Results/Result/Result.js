cf79dc353dee74a57538cddb2494c181bf71d93b
Joe Campbell <joe.campbell@fireman.company>
2023-12-13 10:10:20 +0000

    Update Result.js

--- search-website-functions-v4/client-v4/src/components/Results/Result/Result.js
+++ search-website-functions-v4/client-v4/src/components/Results/Result/Result.js
@@ -16,7 +16,7 @@ export default function Result(props) {
        
         <a href={`/details/${props.document.document.id}`}>            
             <div className="card-body">
-                <h5 className="title-style">{props.document.document.title}</h5>
+                <h5 className="title-style">{props.document.document.id}</h5>
                 <span className="ellipsis" dangerouslySetInnerHTML={createMarkup(props.document.highlights.content)}></span>
                 <br/>
                 <span className="metadata">{props.document.document.category}</span>
